79ace65bcee2a8ea20d61dc49c60126d2e156d5d
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-13 11:15:46 +0000

    Update CalcScript.sh

--- OutofHours/CalcScript.sh
+++ OutofHours/CalcScript.sh
@@ -6,8 +6,8 @@ read -p "But first we must ask you a few questions. (PRESS ENTER)" enter
 echo -e "\n"
 read -p "What is your name: " name
 read -p "How old are you: " age
-
 #This converts the string into int's and performs the (-) operation
+
 diff="$((18 - $age))"
 
 
@@ -15,8 +15,6 @@ if (( $age > 18 ))
 then
 	echo -e "\nChoose an operator"
 	read -p "(x, /, + or -) : " op
-  
-#begin commenting out
 <<com
 	if(( $op -eq "x" || $op -eq "/" || $op -eq "+" || $op -eq "-" )) 
 	then
@@ -30,14 +28,14 @@ then
 		echo -e "\nHey $name, you have input an invalid operator. Goodbye."
 
 	fi
-  
-
 com
-#end of commenting 
 else
-	echo -e "\nHey $name, unfortunately you dont meet the age. "
+	echo -e "\nHey $name, unfortunately you dont meet the age limit \nto use this service. \c"
 	echo -e "Please wait \c" 
 	echo -e "$diff \c"
 	echo -e "more year(s). Goodbye.\n\n"  
 
 fi
+
+
+
