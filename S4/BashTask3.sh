83b20c51f06f565cf52c13812411ff5d9832def3
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-26 12:54:00 +0100

    Update BashTask3.sh

--- S4/BashTask3.sh
+++ S4/BashTask3.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+  GNU nano 4.8                                                                                                     bashtask.sh                                                                                                     Modified  #!/bin/bash
 
 
 
 
 if test -f $1
-then 
-	myArray=(`cat $1`)
+then
+        myArray=(`cat $1`)
         numstr=${#myArray[*]}
-	count=0
-	while test $count -lt $numstr
-	do
-		echo "${myArray[$count]}"
-		((count = $count + 1))  
-	done
-	echo "$1 is a File"
-fi 
+        count=0
+        while test $count -lt $numstr
+        do
+                echo "${myArray[$count]}"
+                ((count = $count + 1))
+        done
+        echo "$1 is a File"
+fi
 
 
 
@@ -24,20 +24,23 @@ j=0
 
 
 while test $i -gt numstr
-do 
-((i=$i+1))
+do
 
-	while test $j -lt ((numstr-$i-1))
-	do
-	((j=$j+1)
-		if test ${myArray[$j]} -gt ${myArray[$(($j+1))]}
-		test
-			#swap
-			temp=${myArray[$j]}
-			myArray[$j]=${myArray[$(($j+1))]}
-			myArray[$(($j+1))]=$temp
-		fi
-	done
+
+        while test $j -lt numstr
+        do
+
+                if test ${myArray[$j]} -gt ${myArray[$(($j+1))]}
+                test
+                        #swap
+                        temp=${myArray[$j]}
+                        myArray[$j]=${myArray[$(($j+1))]}
+                        myArray[$(($j+1))]=$temp
+                fi
+
+        ((i=$i+1))
+        done
+((j=$j+1))
 done
 
 echo "Array in sorted order:"
