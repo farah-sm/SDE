52f679916947302031a01a5b0c5e88621e5b757f
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-26 15:19:08 +0000

    Create BubbleSort(array)-Extra.cs

---
+++ OutofHours/BubbleSort(array)-Extra.cs
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+myArray=($@)
+numstr=${#myArray[*]}
+
+
+i=0
+((numstr = $numstr - 1 ))
+while test $i -lt $numstr
+do
+
+        j=0
+        while test $j -lt $numstr
+        do
+
+                if [[ ${myArray[$j]} > ${myArray[$(($j+1))]} ]]
+                then
+                        echo "swap $j"
+                        temp=${myArray[$j]}
+                        myArray[$j]=${myArray[$j+1]}
+                        myArray[$j+1]=$temp
+                fi
+
+        ((j=$j+1))
+        done
+((i=$i+1))
+done
+
+echo "Array in sorted order:"
+echo ${myArray[*]}
