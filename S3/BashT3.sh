8cdf54cf1a04c0d0e37ced5c7159bd8afe9e8e41
Farah s <65547726+farah-sm@users.noreply.github.com>
2022-10-28 19:48:13 +0000

    Update BashT3.sh

--- S3/BashT3.sh
+++ S3/BashT3.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# This demonstrates the use of an if statement with the condition if the input in positional parameter is a file 
 if test -f $1
 then
+        # If the condition is true then print the following statement
         echo "This is a file"
+
+# If the condition is not true it will then be passed into the else if (elif) part of the statement which checks for the condition if the name input into $1 is a directory
 elif test -d $1
-then
+then    
+        # If this condition is true it will print the following statement
         echo "This is a directory"
 
+# The use of "fi" ends the if statement
 fi
